bee19b7b6636d7f7c721516424d7f93acf4ca176
Speculate Dev <sida3li777@gmail.com>
2025-07-25 11:12:03 +0100

    Store: usePrefrences -- toggle sound enabled

---
+++ app/store/usePrefrences.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+type Prefrences = {
+  soundEnabled: boolean;
+  setSoundEnabled: (soundEnabled: boolean) => void;
+};
+
+export const usePrefrences = create<Prefrences>((set) => ({
+  soundEnabled: true,
+
+  setSoundEnabled: (soundEnabled: boolean) => {
+    set({ soundEnabled });
+  },
+}));
