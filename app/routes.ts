4d0555977a0610c14f8caa097a9c8ed6dc6345ff
Speculate Dev <sida3li777@gmail.com>
2025-07-29 18:59:37 +0100

    Auth-Route: redirection middleware

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,28 @@
-export const protectedRoutes = [];
-export const authRoute = ["/login"];
+/**
+ * An array of routes that are accessible to teh public
+ * These routes do not require auth
+ * @type {string []}
+ */
+
+export const protectedRoutes = ["/"];
+
+/**
+ * An array of routes that are used for auth
+ * these routes will redirect logged in users to /
+ * @type {string[]}
+ */
+export const authRoute = "/auth";
+
+/**
+ * the prefix for API authentication routes
+ * Routes that start with the prefix are used for API auth purposes
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after login
+ * @type {string}
+ */
 export const DEFAULT_REDIRECT = "/";
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,23 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./app/_lib/auth";
+import { authRoute, DEFAULT_REDIRECT, protectedRoutes } from "./app/routes";
+import { NextResponse } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
 // This function can be marked `async` if using `await` inside
 export default auth((req) => {
-  console.log("auth: ", req.auth);
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isAuthRoute = nextUrl.pathname.startsWith(authRoute);
+  const isProtected = protectedRoutes.includes(nextUrl.pathname);
+
+  if (isAuthRoute && isLoggedIn)
+    return NextResponse.redirect(new URL(DEFAULT_REDIRECT, nextUrl.origin));
+
+  if (isProtected && !isLoggedIn)
+    return NextResponse.redirect(new URL(authRoute, nextUrl.origin));
 
   return null;
 });
